ceffe450aa72c27a3a1c62063bf0840401260805
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-05-03 18:42:50 -0500

    new

--- date.js
+++ date.js
@@ -6,16 +6,53 @@ var moment = require('moment');
 var today = moment();
 var day = today.format("YYYY, DD, MM")
 
-var j = schedule.scheduleJob(today.format(), function(){
+// var j = schedule.scheduleJob(today.format(), function(){
+//    pushChanges(day)
+//    day = today.format("YYYY, DD, MM").subtract(1, 'days')    
+// });
 
-console.log('hit')
-   pushChanges(day)
-    
+function dayCommit(date){
+  fs.writeFile("newfile.js",date, function(err) {
+    if(err) {
+      console.log('err1')
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+
+
+}); 
+exec('git add -A',
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('err2')
+      console.log('exec error: ' + error);
+    }
+}).then(function(){
+      exec("git commit --date='"+date+"' -m 'new'",
+  function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      console.log('err3')
+    }
+}).then(function(){
+   exec('git push origin master', function(error, stdout, stderr){
+      console.log('pushed')
+    })
 });
 
+});
+
+day = today.subtract(1, 'days')
+
+}
+
 function pushChanges(date){
-
-
 fs.writeFile("newfile.js",date, function(err) {
     if(err) {
     	console.log('err1')
@@ -58,7 +95,7 @@ exec('git add -A',
 
 
 
-// setInterval(function(){
-//   pushChanges("2015, 1, 2")
-// }, 5000);
+setInterval(function(){
+  dayCommit(day)
+}, 5000);
 
