f1ba67a5ecc40bc975a7b9a766ca051f8ef127c9
GrantSchoffelen <grant.schoffelen@gmail.com>
2015-06-29 20:58:22 -0500

    2015, 29, 06

--- date.js
+++ date.js
@@ -9,6 +9,7 @@ var setDate = moment("2015, 05, 06")
 
 
 function dayCommit(date){ 
+  console.log(today, setDate)
   console.log(date.diff(setDate))
   if(setDate > date ){
     console.log('done')
